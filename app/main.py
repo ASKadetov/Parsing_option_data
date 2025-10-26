aaa1454c88fe0a2a031309a074c9b5d3a945fd1d
ASKadetov <kadetov93@gmail.com>
2025-10-26 20:56:33 +0700

    fix log message

--- app/main.py
+++ app/main.py
@@ -33,7 +33,7 @@ def log_message(message):
     timestamp = datetime.now().strftime('%Y-%m-%d %H:%M:%S')
     log_entry = f"[{timestamp}] {message}\n"
     
-    with open('logs.txt', 'a', encoding='utf-8') as log_file:
+    with open('tmp/logs.txt', 'a', encoding='utf-8') as log_file:
         log_file.write(log_entry)
     print(log_entry)
 
